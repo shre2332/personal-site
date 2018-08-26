7f8a6d9e2441f4f8f6199ef200e4c3a7b7ea0b2d
shre2332 <kurt.shreckengast@gmail.com>
2018-08-25 23:02:22 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { ArtPageComponent } from './art-page/art-page.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ProjectsPageComponent,
+    ArtPageComponent,
+    ResumePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule
